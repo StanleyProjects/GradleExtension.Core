48e0ce06e0c734cac6be571febff736bedb11242
Stanley Wintergreen <kepocnhh@gmail.com>
2023-04-26 19:42:07 +0300

    buildSrc

--- lib/src/main/kotlin/ProjectUtil.kt
+++ lib/src/main/kotlin/ProjectUtil.kt
@@ -1,4 +1,5 @@
 import org.gradle.api.Project
+import sp.gx.core.entity.BuildSrc
 import java.io.File
 
 fun Project.buildDir(file: File): File {
@@ -7,4 +8,33 @@ fun Project.buildDir(file: File): File {
 
 fun Project.buildDir(path: String): File {
     return buildDir(File(path))
+}
+
+fun Project.rootDir(file: File): File {
+    return rootDir.resolve(file)
+}
+
+fun Project.rootDir(path: String): File {
+    return rootDir(File(path))
+}
+
+fun Project.projectDir(file: File): File {
+    return projectDir.resolve(file)
+}
+
+fun Project.projectDir(path: String): File {
+    return projectDir(File(path))
+}
+
+val Project.buildSrc: BuildSrc get() = BuildSrc(
+    projectDir = projectDir("buildSrc"),
+    buildDir = projectDir.resolve("build")
+)
+
+fun Project.buildSrc(file: File): File {
+    return buildSrc.projectDir.resolve(file)
+}
+
+fun Project.buildSrc(path: String): File {
+    return buildSrc(File(path))
 }
---
+++ lib/src/main/kotlin/sp/gx/core/entity/BuildSrc.kt
@@ -0,0 +1,8 @@
+package sp.gx.core.entity
+
+import java.io.File
+
+data class BuildSrc(
+    val projectDir: File,
+    val buildDir: File,
+)
