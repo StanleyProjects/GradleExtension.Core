5b45a11396af1de1e99e11be84fffc87ec103546
Stanley Wintergreen <kepocnhh@gmail.com>
2023-04-26 19:11:41 +0300

    assemble jar

--- buildSrc/src/main/kotlin/Repository.kt
+++ buildSrc/src/main/kotlin/Repository.kt
@@ -1,6 +1,6 @@
 object Repository {
     const val owner = "StanleyProjects"
-    const val name = "ComposeClicks"
+    const val name = "GradleExtension.Core"
 
     fun url(): String {
         return "https://github.com/$owner/$name"
--- lib/build.gradle.kts
+++ lib/build.gradle.kts
@@ -1,3 +1,5 @@
+version = "0.0.1"
+
 repositories.mavenCentral()
 
 plugins {
@@ -9,4 +11,54 @@ plugins {
 
 jacoco {
     toolVersion = Version.jacoco
+}
+
+fun String.join(vararg postfix: String): String {
+    check(isNotEmpty())
+    return postfix.filter { it.isNotEmpty() }.joinToString(separator = "", prefix = this) {
+        it.capitalize()
+    }
+}
+
+@Deprecated(
+    level = DeprecationLevel.ERROR,
+    message = "Use Project.version property.",
+    replaceWith = ReplaceWith("version.toString()"),
+)
+fun Project.version(): String {
+    error("Use Project.version property.")
+}
+
+fun Project.version(vararg postfix: String): String {
+    val prefix = version.toString()
+    check(prefix.isNotEmpty())
+    return postfix.filter { it.isNotEmpty() }.joinToString(separator = "-", prefix = "$prefix-")
+}
+
+tasks.getByName<JavaCompile>("compileJava") {
+    targetCompatibility = Version.jvmTarget
+}
+
+val compileKotlinTask = tasks.getByName<org.jetbrains.kotlin.gradle.tasks.KotlinCompile>("compileKotlin") {
+    kotlinOptions {
+        jvmTarget = Version.jvmTarget
+        freeCompilerArgs = freeCompilerArgs + setOf("-module-name", Maven.groupId + ":" + Maven.artifactId)
+    }
+}
+
+tasks.getByName<JavaCompile>("compileTestJava") {
+    targetCompatibility = Version.jvmTarget
+}
+
+tasks.getByName<org.jetbrains.kotlin.gradle.tasks.KotlinCompile>("compileTestKotlin") {
+    kotlinOptions.jvmTarget = Version.jvmTarget
+}
+
+"snapshot".also { variant ->
+    task<Jar>("assemble".join(variant, "Jar")) {
+        dependsOn(compileKotlinTask)
+        archiveBaseName.set(Maven.artifactId)
+        archiveVersion.set(project.version(variant.toUpperCase()))
+        from(compileKotlinTask.destinationDirectory.asFileTree)
+    }
 }
