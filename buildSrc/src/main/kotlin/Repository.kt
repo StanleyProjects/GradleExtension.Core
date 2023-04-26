35864cde7a1c8f60fa1fcb295245b5cb63aa8010
Stanley Wintergreen <kepocnhh@gmail.com>
2023-04-26 18:48:46 +0300

    init gradle

---
+++ buildSrc/src/main/kotlin/Repository.kt
@@ -0,0 +1,8 @@
+object Repository {
+    const val owner = "StanleyProjects"
+    const val name = "ComposeClicks"
+
+    fun url(): String {
+        return "https://github.com/$owner/$name"
+    }
+}
