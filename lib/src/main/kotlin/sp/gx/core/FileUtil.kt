4ba861553cad4c80c00187d14002e2fd370306a4
Stanley Wintergreen <kepocnhh@gmail.com>
2023-05-10 21:51:33 +0300

    check file

--- lib/src/main/kotlin/sp/gx/core/FileUtil.kt
+++ lib/src/main/kotlin/sp/gx/core/FileUtil.kt
@@ -1,5 +1,6 @@
 package sp.gx.core
 
+import org.gradle.api.Project
 import java.io.File
 
 /**
@@ -37,4 +38,42 @@ fun File.file(): File {
 fun File.filled(): File {
     check(length() > 0)
     return this
+}
+
+fun checkFile(name: String, file: File, expected: Set<String>, report: File) {
+    val issues = when {
+        !file.exists() -> setOf("the file does not exist")
+        file.isDirectory -> setOf("the file is a directory")
+        else -> {
+            val actual = file.readLines(Charsets.UTF_8)
+            when {
+                actual.isEmpty() -> setOf("the file does not contain text")
+                else -> {
+                    expected.mapNotNull { e ->
+                        "the file does not contain \"$e\" line".takeIf { _ ->
+                            actual.none { it.contains(e) }
+                        }
+                    }.toSet()
+                }
+            }
+        }
+    }
+    when {
+        report.exists() -> report.delete()
+        else -> report.parentFile?.mkdirs()
+    }
+    if (issues.isEmpty()) {
+        val message = "All checks of the file along the \"${file.name}\" were successful."
+        report.writeText(message)
+        println(message)
+        return
+    }
+    val text = """
+        <html>
+        <h3>The following problems were found while checking the <code>${file.name}</code>:</h3>
+        ${issues.joinToString(prefix = "<ul>", postfix = "</ul>", separator = "\n") { "<li>$it</li>" }}
+        </html>
+    """.trimIndent()
+    report.writeText(text)
+    error("Problems were found while checking the \"${file.name}\". See the report ${report.absolutePath}")
 }
