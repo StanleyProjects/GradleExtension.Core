8b7ea927b4245cdf9aa85fe31a6bc2799a14f555
Stanley Wintergreen <kepocnhh@gmail.com>
2023-05-10 16:11:46 +0300

    file util

--- lib/src/main/kotlin/sp/gx/core/FileUtil.kt
+++ lib/src/main/kotlin/sp/gx/core/FileUtil.kt
@@ -12,3 +12,28 @@ fun File.existing(): File {
     check(exists())
     return this
 }
+
+/**
+ * ```
+ * val foo = File("/tmp/bar").existing().file().filled()
+ * ```
+ * @return [this] receiver [File].
+ * @throws IllegalStateException if [this] receiver [File] is not a normal file.
+ * @author [Stanley Wintergreen](https://github.com/kepocnhh)
+ * @since 0.1.1
+ */
+fun File.file(): File {
+    check(isFile)
+    return this
+}
+
+/**
+ * @return [this] receiver [File].
+ * @throws IllegalStateException if [this] receiver [File] is empty.
+ * @author [Stanley Wintergreen](https://github.com/kepocnhh)
+ * @since 0.1.1
+ */
+fun File.filled(): File {
+    check(length() > 0)
+    return this
+}
