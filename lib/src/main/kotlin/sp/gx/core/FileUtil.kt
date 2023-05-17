ff88a54c4e3b718ae82e02a4f382b4b1f4087fa8
Stanley Wintergreen <kepocnhh@gmail.com>
2023-05-17 16:27:24 +0300

    assemble file

--- lib/src/main/kotlin/sp/gx/core/FileUtil.kt
+++ lib/src/main/kotlin/sp/gx/core/FileUtil.kt
@@ -50,64 +50,13 @@ fun File.filled(): File {
     return this
 }
 
-/**
- * Usage:
- * ```
- * File("/tmp/bar").check(
- *     expected = setOf("foo", "bar"),
- *     report = File("/tmp/report"),
- * )
- * ```
- * @receiver The [File] whose contents will be checked.
- * @param expected The set of strings expected to be in [this] receiver [File].
- * @param report Where the test result will be written.
- * @throws IllegalStateException if [this] receiver [File] does not exist.
- * @throws IllegalStateException if [this] receiver [File] is a directory.
- * @throws IllegalStateException if [this] receiver [File] is empty.
- * @throws IllegalStateException if [this] receiver [File] does not contain text.
- * @throws IllegalStateException if [report] exists and not a file.
- * @throws IllegalStateException if problems are found while checking [this] receiver [File].
- * @author [Stanley Wintergreen](https://github.com/kepocnhh)
- * @since 0.2.0
- */
-fun File.check(expected: Set<String>, report: File) {
-    val issues = when {
-        !exists() -> setOf("the file does not exist")
-        isDirectory -> setOf("the file is a directory")
-        else -> {
-            val actual = readLines(Charsets.UTF_8)
-            if (actual.isEmpty()) {
-                setOf("the file does not contain text")
-            } else {
-                expected.mapNotNull { line ->
-                    if (actual.none { it.contains(line) }) {
-                        "the file does not contain \"$line\" line"
-                    } else {
-                        null
-                    }
-                }.toSet()
-            }
-        }
-    }
-    if (report.exists()) {
-        check(report.isFile) { "report is not a file" }
-        check(report.delete())
+fun File.assemble(text: String) {
+    check(text.isNotEmpty())
+    if (exists()) {
+        check(isFile)
+        check(delete())
     } else {
-        report.parentFile?.mkdirs() ?: error("report has no parent")
+        parentFile?.mkdirs()
     }
-    if (issues.isEmpty()) {
-        val message = "All checks of the file along the \"$name\" were successful."
-        report.writeText(message)
-        @Suppress("ForbiddenMethodCall")
-        println(message)
-        return
-    }
-    val text = """
-        <html>
-        <h3>The following problems were found while checking the <code>$name</code>:</h3>
-        ${issues.joinToString(prefix = "<ul>", postfix = "</ul>", separator = "\n") { "<li>$it</li>" }}
-        </html>
-    """.trimIndent()
-    report.writeText(text)
-    error("Problems were found while checking the \"$name\". See the report ${report.absolutePath}")
+    writeText(text)
 }
