0d70296964a41a353b8bc7ba3400181ebdd424d6
Stanley Wintergreen <kepocnhh@gmail.com>
2023-05-18 16:30:05 +0300

    test badge

---
+++ lib/src/test/kotlin/sp/gx/core/BadgeTest.kt
@@ -0,0 +1,58 @@
+package sp.gx.core
+
+import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.Test
+
+internal class BadgeTest {
+    @Test
+    fun urlTest() {
+        val actual = Badge.url(
+            label = "foo",
+            message = "bar",
+            color = "baz",
+        )
+        val params = "label=foo&message=bar&labelColor=212121&color=baz&style=flat"
+        Assertions.assertEquals("https://img.shields.io/static/v1?$params", actual.toString())
+    }
+
+    @Test
+    fun urlErrorTest() {
+        Assertions.assertThrows(IllegalStateException::class.java) {
+            Badge.url(
+                label = "",
+                message = "",
+                color = "",
+            )
+        }
+        Assertions.assertThrows(IllegalStateException::class.java) {
+            Badge.url(
+                label = "foo",
+                message = "",
+                color = "",
+            )
+        }
+        Assertions.assertThrows(IllegalStateException::class.java) {
+            Badge.url(
+                label = "foo",
+                message = "bar",
+                labelColor = "",
+                color = "",
+            )
+        }
+        Assertions.assertThrows(IllegalStateException::class.java) {
+            Badge.url(
+                label = "foo",
+                message = "bar",
+                color = "",
+            )
+        }
+        Assertions.assertThrows(IllegalStateException::class.java) {
+            Badge.url(
+                label = "foo",
+                message = "bar",
+                color = "baz",
+                style = "",
+            )
+        }
+    }
+}
