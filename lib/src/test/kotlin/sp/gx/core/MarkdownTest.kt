1e485810106690120a49ae1e200f699a1ef1ffdc
Stanley Wintergreen <kepocnhh@gmail.com>
2023-05-18 16:34:30 +0300

    test markdown

---
+++ lib/src/test/kotlin/sp/gx/core/MarkdownTest.kt
@@ -0,0 +1,35 @@
+package sp.gx.core
+
+import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.Test
+import java.net.URL
+
+internal class MarkdownTest {
+    @Test
+    fun urlTest() {
+        val actual = Markdown.url(
+            text = "foo",
+            value = URL("https://github.com")
+        )
+        Assertions.assertEquals("[foo](https://github.com)", actual)
+    }
+
+    @Test
+    fun urlErrorTest() {
+        Assertions.assertThrows(IllegalStateException::class.java) {
+            Markdown.url(
+                text = "",
+                value = URL("https://github.com")
+            )
+        }
+    }
+
+    @Test
+    fun imageTest() {
+        val actual = Markdown.image(
+            text = "foo",
+            path = URL("https://github.com")
+        )
+        Assertions.assertEquals("![foo](https://github.com)", actual)
+    }
+}
