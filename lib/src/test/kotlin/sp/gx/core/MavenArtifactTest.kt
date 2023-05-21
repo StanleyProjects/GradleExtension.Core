c907d0d40cae6bc6d869f761a15871ec8cf69ded
Stanley Wintergreen <kepocnhh@gmail.com>
2023-05-21 15:29:56 +0300

    test maven artifact

---
+++ lib/src/test/kotlin/sp/gx/core/MavenArtifactTest.kt
@@ -0,0 +1,57 @@
+package sp.gx.core
+
+import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.Test
+
+internal class MavenArtifactTest {
+    @Test
+    fun constructorTest() {
+        val groupId = "foo"
+        val artifactId = "bar"
+        val artifact = Maven.Artifact(group = groupId, id = artifactId)
+        Assertions.assertEquals(groupId, artifact.group)
+        Assertions.assertEquals(artifactId, artifact.id)
+    }
+
+    @Test
+    fun constructorErrorTest() {
+        Assertions.assertThrows(IllegalStateException::class.java) {
+            Maven.Artifact(group = "", id = "")
+        }
+        Assertions.assertThrows(IllegalStateException::class.java) {
+            Maven.Artifact(group = "foo", id = "")
+        }
+    }
+
+    @Test
+    fun toStringTest() {
+        val groupId = "foo"
+        val artifactId = "bar"
+        val artifact = Maven.Artifact(group = groupId, id = artifactId)
+        Assertions.assertEquals("Artifact($groupId/$artifactId)", artifact.toString())
+    }
+
+    @Test
+    fun hashCodeTest() {
+        val groupId = "foo"
+        val artifactId = "bar"
+        val artifact = Maven.Artifact(group = groupId, id = artifactId)
+        artifact.hashCode()
+    }
+
+    @Test
+    fun equalsTest() {
+        val groupId = "foo"
+        val artifactId = "bar"
+        val first = Maven.Artifact(group = groupId, id = artifactId)
+        val second = Maven.Artifact(group = groupId, id = artifactId)
+        Assertions.assertTrue(first == second)
+    }
+
+    @Test
+    fun equalsNotTest() {
+        Assertions.assertFalse(Maven.Artifact(group = "foo", id = "42") == Maven.Artifact(group = "bar", id = "42"))
+        Assertions.assertFalse(Maven.Artifact(group = "42", id = "foo") == Maven.Artifact(group = "42", id = "bar"))
+        Assertions.assertFalse(Maven.Artifact(group = "foo", id = "42").equals(Unit))
+    }
+}
