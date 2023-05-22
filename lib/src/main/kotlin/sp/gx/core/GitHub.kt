5f49749b15a5a72dc5cc8df9d9e82428078cdb78
Stanley Wintergreen <kepocnhh@gmail.com>
2023-05-22 21:24:33 +0300

    gh repository

--- lib/src/main/kotlin/sp/gx/core/GitHub.kt
+++ lib/src/main/kotlin/sp/gx/core/GitHub.kt
@@ -2,8 +2,29 @@ package sp.gx.core
 
 import java.net.URI
 import java.net.URL
+import java.util.Objects
 
 object GitHub {
+    class Repository(val owner: String, val name: String) {
+        init {
+            check(owner.isNotEmpty())
+            check(name.isNotEmpty())
+        }
+
+        override fun toString(): String {
+            return "Repository($owner/$name)"
+        }
+
+        override fun hashCode(): Int {
+            return Objects.hash(owner, name)
+        }
+
+        override fun equals(other: Any?): Boolean {
+            if (other !is Repository) return false
+            return owner == other.owner && name == other.name
+        }
+    }
+
     fun pages(
         owner: String,
         name: String,
