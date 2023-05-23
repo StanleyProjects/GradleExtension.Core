29c5a47ecca145174e36ca07c81bcb8c44c21ba5
Stanley Wintergreen <kepocnhh@gmail.com>
2023-05-23 19:40:28 +0300

    URL utils

--- lib/src/main/kotlin/sp/gx/core/URLUtil.kt
+++ lib/src/main/kotlin/sp/gx/core/URLUtil.kt
@@ -2,6 +2,20 @@ package sp.gx.core
 
 import java.net.URL
 
-fun URL.resolve(): URL {
-    TODO()
+/**
+ * Usage:
+ * ```
+ * val url = URL("https://github.com").resolve("foo")
+ * assertEquals(URL("https://github.com/foo"), url)
+ * ```
+ * @return concatenated [this] and [relative] paths.
+ * @author [Stanley Wintergreen](https://github.com/kepocnhh)
+ * @since 0.3.0
+ */
+fun URL.resolve(relative: String): URL {
+    val spec = StringBuilder(toString())
+        .append("/")
+        .append(relative)
+        .toString()
+    return URL(spec)
 }
