9dd69a77de5db0903f97f6ebe573b5ce4b72c2bf
Stanley Wintergreen <kepocnhh@gmail.com>
2024-02-22 18:09:40 +0300

    unit test project

--- lib/src/test/kotlin/sp/gx/core/DirectoryUtilTest.kt
+++ lib/src/test/kotlin/sp/gx/core/DirectoryUtilTest.kt
@@ -7,7 +7,7 @@ import java.nio.file.Files
 
 internal class DirectoryUtilTest {
     @Test
-    fun fooTest() {
+    fun asFileTest() {
         val projectDir = Files.createTempDirectory("unittest").toFile()
         val project = ProjectBuilder.builder().withProjectDir(projectDir).build()
         check(projectDir.resolve("foo").mkdirs())
