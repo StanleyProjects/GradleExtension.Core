9c2ebdc2acf44832a0e1d933da4728299aee363b
Stanley Wintergreen <kepocnhh@gmail.com>
2024-02-25 03:37:24 +0300

    tasks

--- lib/src/main/kotlin/sp/gx/core/TaskUtil.kt
+++ lib/src/main/kotlin/sp/gx/core/TaskUtil.kt
@@ -2,6 +2,7 @@ package sp.gx.core
 
 import org.gradle.api.Project
 import org.gradle.api.Task
+import org.gradle.api.tasks.TaskContainer
 
 inline fun <reified T : Task> Project.task(
     nameSegment: String,
@@ -39,4 +40,28 @@ fun Project.task(
 ): Task {
     val name = camelCase(camelCase(nameSegment, secondNameSegment), *otherNameSegments)
     return tasks.create(name)
+}
+
+inline fun <reified T : Task> TaskContainer.getByName(
+    nameSegment: String,
+    secondNameSegment: String,
+    vararg otherNameSegments: String,
+    noinline block: T.() -> Unit,
+): T {
+    val name = camelCase(camelCase(nameSegment, secondNameSegment), *otherNameSegments)
+    val task = getByName(name)
+    check(task is T)
+    task.block()
+    return task
+}
+
+inline fun <reified T : Task> TaskContainer.getByName(
+    nameSegment: String,
+    secondNameSegment: String,
+    vararg otherNameSegments: String,
+): T {
+    val name = camelCase(camelCase(nameSegment, secondNameSegment), *otherNameSegments)
+    val task = getByName(name)
+    check(task is T)
+    return task
 }
