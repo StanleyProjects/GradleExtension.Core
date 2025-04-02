e4c08d1324a8e43956a103d848777e5349a0a5e3
Stanley Wintergreen <kepocnhh@gmail.com>
2025-04-02 18:14:14 +0300

    jupiter:5.11.4

--- buildSrc/src/main/kotlin/Version.kt
+++ buildSrc/src/main/kotlin/Version.kt
@@ -4,8 +4,8 @@ object Version {
     const val detekt = "1.23.6"
     const val dokka = "1.9.20"
     const val jacoco = "0.8.12"
-    const val jupiter = "5.10.2"
-    val jvmTarget = JavaVersion.VERSION_1_8
+    const val jupiter = "5.11.4"
+    val jvmTarget = JavaVersion.VERSION_17
     const val kotlin = "1.9.20"
     const val ktlint = "0.50.0"
 }
