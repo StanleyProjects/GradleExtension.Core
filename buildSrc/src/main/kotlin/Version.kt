a5c5fb0cc7bb3d03fac6dfb9480a036b3c038798
Stanley Wintergreen <kepocnhh@gmail.com>
2025-04-02 17:56:07 +0300

    jacoco:0.8.12

--- buildSrc/src/main/kotlin/Version.kt
+++ buildSrc/src/main/kotlin/Version.kt
@@ -3,7 +3,7 @@ import org.gradle.api.JavaVersion
 object Version {
     const val detekt = "1.23.6"
     const val dokka = "1.9.20"
-    const val jacoco = "0.8.11"
+    const val jacoco = "0.8.12"
     const val jupiter = "5.10.2"
     val jvmTarget = JavaVersion.VERSION_1_8
     const val kotlin = "1.9.20"
