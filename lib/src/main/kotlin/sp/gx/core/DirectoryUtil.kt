4020473f8aa9d9dce5474c4a9e7d96f865191651
Stanley Wintergreen <kepocnhh@gmail.com>
2025-04-03 16:09:20 +0300

    fix doc

--- lib/src/main/kotlin/sp/gx/core/DirectoryUtil.kt
+++ lib/src/main/kotlin/sp/gx/core/DirectoryUtil.kt
@@ -19,6 +19,23 @@ fun Directory.asFile(path: String): File {
     return file(path).asFile
 }
 
+/**
+ * Usage:
+ * ```
+ * val project: Project = ...
+ * val file = project.layout.projectDirectory.eff("foo")
+ * assertTrue(file.exists())
+ * assertTrue(file.isFile)
+ * assertTrue(file.length() > 0)
+ * ```
+ *
+ * @return a [File] whose value is a [Directory] whose location is the given path resolved relative to this directory.
+ * @throws IllegalStateException if target [File] does not exist.
+ * @throws IllegalStateException if target [File] is not a normal file.
+ * @throws IllegalStateException if target [File] is empty.
+ * @author [Stanley Wintergreen](https://github.com/kepocnhh)
+ * @since 0.6.1
+ */
 fun Directory.eff(path: String): File {
     val file = file(path).asFile
     if (!file.exists()) {
